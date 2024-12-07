f5bda80695a3e3061403f6e965f9fc514d492d43
Domingos chiconela <azevedodomingos78@gmail.com>
2024-12-07 10:15:49 +0200

    feat: added new pages to learn and practice routing concepts

---
+++ 02-criando-project/src/app/Products/Categories/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Categories ()  {
+
+    return(
+    <h1 className="text-blue-600 text-lg flex justify-center">pagina de categorias</h1>
+    )
+    }
+   
---
+++ 02-criando-project/src/app/Products/page.tsx
@@ -0,0 +1,9 @@
+
+//por convencao o next faz a exportacao por default e nao nomeada sendo assim arrow function tenho que fazer a exportacao depois por default pois nao posso fazer a nomeada na mesma linha 
+ const Products = () => {
+
+return(
+<h1 className="text-blue-600 text-lg flex justify-center">pagina de productos</h1>
+)
+}
+export default Products;
